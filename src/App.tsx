977759911509dbae891d4f7544f48122d5963fb8
Ritesh Gupta <rishup132@Riteshs-MacBook-Pro.local>
2024-03-16 15:39:49 +0530

    Added the upload file functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import './App.css'
-// import Home from './tab/home.tsx'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './tab/home.tsx'
+import UploadPage from './tab/uploadPage.tsx'
 
 function App() {
   return (
-    <>
-    <h1>Hello</h1>
-      {/* <Home /> */}
-    </>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/upload">Upload</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/upload">
+            <UploadPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
--- src/tab/uploadPage.tsx
+++ src/tab/uploadPage.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UploadPage: React.FC = () => {
+
+  const [fileUrl, setFileUrl] = useState<string | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        const url = fileReader.result as string;
+        setFileUrl(url);
+      };
+      fileReader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div>
       <h2>Upload Page</h2>
-      <p>This is the upload page where you can upload a file.</p>
-      <p>Render the file here.</p>
-      {/* Add your file upload component here */}
+      <div>
+      <input type="file" onChange={handleFileChange} />
+      {fileUrl && (
+        <div>
+          <h2>Preview:</h2>
+          <iframe src={fileUrl} width="500px" height="500px" title="File Preview" />
+        </div>
+      )}
+    </div>
     </div>
   );
 };
+
+export default UploadPage;
