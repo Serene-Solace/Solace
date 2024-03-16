7dc8d4b963f0d0f004084b20794dd7532de2b1df
Ritesh Gupta <rishup132@Riteshs-MacBook-Pro.local>
2024-03-16 16:54:45 +0530

    Added the storage layer and render the pdf.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import './App.css'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './tab/home.tsx'
 import UploadPage from './tab/uploadPage.tsx'
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { type AuthUser } from "aws-amplify/auth";
+import { type UseAuthenticator } from "@aws-amplify/ui-react-core";
+import React from 'react';
 
-function App() {
+type AppProps = {
+  signOut?: UseAuthenticator["signOut"]; //() => void;
+  user?: AuthUser;
+};
+
+const App: React.FC<AppProps> = ({signOut, user}) => {
   return (
     <Router>
       <div>
+        <Heading level={1}>Hello {user.username}</Heading>
+        <Button onClick={signOut}>Sign out</Button>
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -29,4 +41,4 @@ function App() {
   )
 }
 
-export default App
+export default withAuthenticator(App);
