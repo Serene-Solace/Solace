362fe899cc18334c5887cbd824a285a95a50427b
Ritesh Gupta <rishup132@Riteshs-MacBook-Pro.local>
2024-03-17 20:53:32 +0530

    Removed build errors in production.

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Navbar() {
  return (
@@ -12,7 +12,7 @@ function Navbar() {
  );
 }
 
-function CustomLink({ to, children, ...props }) {
+function CustomLink({ to, children, ...props } : {to:any, children:any}) {
   
     return (
       <li>
