d245871af06d5c245162eb7a9851af848822c07b
Ritesh Gupta <rishup132@Riteshs-MacBook-Pro.local>
2024-03-17 19:57:53 +0530

    Added code changes for define functionality.

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,21 @@ export default defineConfig({
        },
      ],
    }),
-  ]
+  ],
+  ...(process.env.NODE_ENV === 'development'
+    ? {
+      define: {
+        global: {},
+      },
+    }
+    : {}),
+  resolve: {
+    alias: {
+      ...(process.env.NODE_ENV !== 'development'
+        ? {
+          './runtimeConfig': './runtimeConfig.browser', //fix production build
+        }
+        : {}),
+    },
+  },
 });
