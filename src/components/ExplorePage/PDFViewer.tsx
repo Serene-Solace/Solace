2d2b3fbbabd9821c3f357f060e11db17467ea56b
Ritesh Gupta <rishup132@Riteshs-MacBook-Pro.local>
2024-03-24 04:25:15 +0530

    Added the popup related changes.

--- src/components/ExplorePage/PDFViewer.tsx
+++ src/components/ExplorePage/PDFViewer.tsx
@@ -1,24 +1,116 @@
-import { Grid } from "@mui/material";
+import { useCallback, useState, useEffect } from 'react';
+import { useResizeObserver } from '@wojtekmaj/react-hooks';
+import { pdfjs, Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import '../../css/Sample.css';
+import type { PDFDocumentProxy } from 'pdfjs-dist';
+import PopupWithButton from './PopupWithButton';
 
-type PDFViewerSectionProps = {
-    pdfFileURL: any;
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
+
+const options = {
+  cMapUrl: '/cmaps/',
+  standardFontDataUrl: '/standard_fonts/',
+};
+
+const resizeObserverOptions = {};
+
+const maxWidth = 800;
+
+type ChildProps = {
+  file: File | undefined
 }
 
-const PDFViewerSection: React.FC<PDFViewerSectionProps> = (props) => {
-    return (
-        <>
-            <Grid>
-                <div style={{ display: "flex" }}>
-                    <iframe
-                        title="S3 Content"
-                        src={props.pdfFileURL}
-                        width="700px"
-                        height="550px"
-                    ></iframe>
-                </div>
-            </Grid>
-        </>
-    );
+const PDFViewer: React.FC<ChildProps> = (props) => {
+
+  const [numPages, setNumPages] = useState<number>();
+  const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>();
+  const [selectedText, setSelectedText] = useState<undefined | String>('');
+  const [contextMenuPosition, setContextMenuPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleTextSelection = () => {
+      const selection = window.getSelection();
+      if (selection && selection.toString().length > 0) {
+        const range = selection.getRangeAt(0).getBoundingClientRect();
+        setContextMenuPosition({
+          x: range.right,
+          y: range.bottom,
+        });
+        setSelectedText(selection.toString().trim());
+      } else {
+        setSelectedText('');
+      }
+    };
+
+    document.addEventListener('mouseup', handleTextSelection);
+
+    return () => {
+      document.removeEventListener('mouseup', handleTextSelection);
+    };
+  }, []);
+
+  const onResize = useCallback<ResizeObserverCallback>((entries) => {
+    const [entry] = entries;
+
+    if (entry) {
+      setContainerWidth(entry.contentRect.width);
+    }
+  }, []);
+
+  useResizeObserver(containerRef, resizeObserverOptions, onResize);
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void {
+    setNumPages(nextNumPages);
+  }
+
+  const onItemClick = useCallback((args: { pageNumber: number }) => {
+    console.log('Clicked an item', args);
+  }, []);
+
+  function handleMouseUp(): void {
+    const selectedText = window.getSelection()?.toString();
+    if (selectedText?.trim() != "") {
+      console.log(`Selected text: ${selectedText}`);
+      setSelectedText(selectedText);
+    }
+  }
+
+  return (
+    <div className="Example">
+      <div onMouseUp={handleMouseUp} className={selectedText != "" ? "Example__container_1" : "Example__container"}>
+        <div className="Example__container__document" ref={setContainerRef}>
+          <Document file={props.file}
+            onLoadSuccess={onDocumentLoadSuccess}
+            onItemClick={onItemClick}
+            options={options}>
+            {Array.from(new Array(numPages),
+              // @ts-ignore 
+              (el, index) => (
+                <Page
+                  key={`page_${index + 1}`}
+                  pageNumber={index + 1}
+                  width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
+                />
+              ))}
+          </Document>
+        </div>
+      </div>
+      {selectedText != undefined && selectedText != "" && 
+        <PopupWithButton 
+          top={contextMenuPosition.y} 
+          left={contextMenuPosition.x}
+          fileName={props.file?.name}
+          text={selectedText}
+        />
+      }
+    </div>
+  );
 }
 
-export default PDFViewerSection;
+export default PDFViewer;
---
+++ src/components/ExplorePage/PopupWithButton.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import Toolbar from '../../tab/toolBar';
+import { View } from '@aws-amplify/ui-react';
+import './ExplorePage.css';
+
+type ChildProps = {
+  text: any;
+  fileName: any;
+  top: any;
+  left: any;
+}
+
+const PopupWithButton: React.FC<ChildProps> = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState('');
+
+  const handleExplore = async () => {
+    setLoading(true);
+    try {
+      const apiKey = import.meta.env.VITE_API_GATEWAY_KEY_ID;
+      const response = await fetch('https://kahou06ob0.execute-api.ap-south-1.amazonaws.com/default/openai_invoke_api', {
+        method: 'POST',
+        headers: {
+          'x-api-key': apiKey,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          text: props.text,
+          fileName: props.fileName
+        })
+      });
+      const data = await response.json();
+      setResult(data['body']['response']);
+    } catch (error) {
+      console.error('Error fetching content:', error);
+      setResult('Error fetching content');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      {result == '' && (
+        <div
+          className="popup"
+          style={{
+            position: 'fixed',
+            top: props.top,
+            left: props.left,
+          }}
+        >
+          <Toolbar _invokeLambda={() => handleExplore()} _isLoading={loading} />
+        </div>
+      )}
+      {result != '' && (
+          <View
+                as="div"
+                className='rectangle'
+                style={{
+                  position: 'fixed',
+                  top: props.top,
+                  left: props.left,
+                }}
+                display="flex"
+                width="fit-content"
+                height="fit-content"
+                ariaLabel="View example"
+                backgroundColor="var(--amplify-colors-white)"
+                borderRadius="6px"
+                border="1px solid var(--amplify-colors-black)"
+                color="var(--amplify-colors-blue-60)"
+                maxWidth="20%"
+                padding="1rem"
+                onClick={() => alert('🏔 What a beautiful <View>! 🔭')}
+              >
+                {result}
+              </View>
+      )}
+    </>
+  );
+};
+
+export default PopupWithButton;
